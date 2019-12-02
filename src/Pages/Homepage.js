13d01d1c9922f535cd36fafdd5838f6094c11b9b
BosEriko <bos.eriko@gmail.com>
2019-12-02 23:00:38 +0800

    [AUTO] See last commit

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,15 +1,37 @@
 import React from 'react'
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet"
+import Axios from 'axios'
 
-const Homepage = () => {
-  return (
-    <div className="my-5">
-      <Helmet>
-        <title>Homepage</title>
-      </Helmet>
-      Homepage
-    </div>
+class Homepage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      artists: []
+    }
+  }
+  componentDidMount() {
+    Axios.get('http://localhost:4000/artist')
+      .then((response) => {
+        this.setState({
+          artists: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+  render() {
+    return (
+      <div className="my-5">
+        <Helmet>
+          <title>Homepage</title>
+        </Helmet>
+        {this.state.artists.map((artist, index) => {
+          return (<div key={index}>{ artist.name }</div>)
+        })}
+      </div>
     )
+  }
 }
 
 export default Homepage;
