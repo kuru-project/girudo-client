791385935b06693fb50c4c5c53aba5ea1c04cc19
BosEriko <bos.eriko@gmail.com>
2019-12-02 16:30:15 +0800

    add state events

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Login = () => {
-  return (
-    <div>
-      Login page
-    </div>
+class Login extends React.Component {
+  handleChange(event) {
+    console.log("Input is being updated")
+  }
+  handleSubmit(event) {
+    console.log("You submitted the data!")
+    event.preventDefault()
+  }
+  render() {
+    return(
+      <div>
+        <input onChange={this.handleChange} name="email" type="email" />
+        <input onChange={this.handleChange} name="password" type="password" />
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>
     )
+  }
 }
 
 export default Login;
