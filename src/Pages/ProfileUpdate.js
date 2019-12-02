8e5b5c5d6d476d904b6292bad4e6c7efac98a5a2
BosEriko <bos.eriko@gmail.com>
2019-12-02 22:27:20 +0800

    [AUTO] small is a real HTML tag, who knew.

--- src/Pages/ProfileUpdate.js
+++ src/Pages/ProfileUpdate.js
@@ -7,7 +7,9 @@ class ProfileUpdate extends React.Component {
     super(props)
     this.state = {
       name: '',
-      skills: ''
+      skills: '',
+      isAdmin: false,
+      isArtist: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -20,9 +22,12 @@ class ProfileUpdate extends React.Component {
   }
 
   handleSubmit(event) {
+    console.log(this.state)
     Axios.patch(`http://localhost:4000/user/${this.props.match.params.user_id}/update`, {
       'name': this.state.name,
-      'skills': [this.state.skills]
+      'skills': [this.state.skills],
+      'isAdmin': this.state.isAdmin === "on" ? true : false,
+      'isArtist': this.state.isArtist === "on" ? true : false
     }, {
       headers: {
         'x-auth-token': sessionStorage.token
@@ -43,6 +48,22 @@ class ProfileUpdate extends React.Component {
           <title>Update Profile</title>
         </Helmet>
         <form>
+          <label>
+            Is admin:
+            <input
+              name="isAdmin"
+              type="checkbox"
+              checked={this.state.isAdmin}
+              onChange={this.handleChange} />
+          </label>
+          <label>
+            Is artist:
+            <input
+              name="isArtist"
+              type="checkbox"
+              checked={this.state.isArtist}
+              onChange={this.handleChange} />
+          </label>
           <input className={inputFieldStyle} onChange={this.handleChange} name="name" placeholder="Name" />
           <input className={inputFieldStyle} onChange={this.handleChange} name="skills" placeholder="Skills" />
           <button className={buttonStyle} onClick={this.handleSubmit}>Submit</button>
