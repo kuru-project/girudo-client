7f944477534f58260c0d69cdb0b8b40338b704af
BosEriko <bos.eriko@gmail.com>
2019-12-03 03:15:24 +0800

    [AUTO] Too lazy to write descriptive message

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -7,8 +7,9 @@ class Book extends React.Component {
     super(props)
     this.state = {
       timestamp: 1,
-      artist_id: '',
-      booker_id: '',
+      contactNumber: 0,
+      artistId: '',
+      bookerId: '',
       location: ''
     }
     this.handleChange = this.handleChange.bind(this)
@@ -25,8 +26,9 @@ class Book extends React.Component {
     console.log(this.state)
     Axios.post(`http://localhost:4000/book/new`, {
       'timestamp': this.state.timestamp,
-      'artist_id': this.state.artist_id,
-      'booker_id': this.state.booker_id,
+      'contactNumber': this.state.contactNumber,
+      'artistId': this.state.artistId,
+      'bookerId': this.state.bookerId,
       'location': this.state.location,
     }, {
       headers: {
@@ -43,14 +45,18 @@ class Book extends React.Component {
   render() {
     const inputFieldStyle = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-5"
     const buttonStyle     = "block w-full text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    // eslint-disable-next-line
+    const user = eval('(' + sessionStorage.user + ')');
     return (
       <div className="my-5">
         <Helmet>
           <title>Book</title>
         </Helmet>
         <form>
-          <input className={inputFieldStyle} onChange={this.handleChange} name="artist_id" placeholder="Artist ID" />
-          <input className={inputFieldStyle} onChange={this.handleChange} name="booker_id" placeholder="Booker ID" />
+          <input className={inputFieldStyle} onChange={this.handleChange} name="timestamp" placeholder="Timestamp" />
+          <input className={inputFieldStyle} onChange={this.handleChange} name="contactNumber" value={user.contactNumber} />
+          <input className={inputFieldStyle} onChange={this.handleChange} name="artistId" value={this.props.match.params.user_id} />
+          <input className={inputFieldStyle} onChange={this.handleChange} name="bookerId" value={user.id} />
           <input className={inputFieldStyle} onChange={this.handleChange} name="location" placeholder="Location" />
           <button className={buttonStyle} onClick={this.handleSubmit}>Submit</button>
         </form>
