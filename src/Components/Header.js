f15faca0512b1d4a662acb277053a8f39870d21b
BosEriko <bos.eriko@gmail.com>
2019-12-02 20:21:21 +0800

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Display from './Display'
+import { Link } from 'react-router-dom'
 
 class Header extends React.Component {
   constructor(props) {
@@ -19,9 +20,16 @@ class Header extends React.Component {
     return(
       <header className="bg-red-700 py-5 text-white">
         <div className="container mx-auto flex">
-          <div className="mr-auto">this is the logo</div>
+          <div className="mr-auto">
+            <Link className="hover:underline" to="/"><h1>Maven</h1></Link>
+          </div>
           <div>
-            This is the header
+            <Display toggle={!(this.state.sessionStorage.length)}>
+              <ul className="flex">
+                <li className="mr-3"><Link className="hover:underline" to="/login">Login</Link></li>
+                <li><Link className="hover:underline" to="/register">Register</Link></li>
+              </ul>
+            </Display>
             <Display toggle={this.state.sessionStorage.length}>
               <button onClick={this.logOut}>
                 Log Out
