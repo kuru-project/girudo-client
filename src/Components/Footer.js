5b0eefb6a8abe1b13da7215f27eb47b287254e6b
BosEriko <bos.eriko@gmail.com>
2019-12-02 18:21:48 +0800

    [AUTO] yet another quality commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Display from './Display'
 
-const Footer = () => {
-	return(
-    <footer>
-      <Link to="/">Home</Link> |
-      <Link to="/login">Login</Link> |
-      <Link to="/register">Register</Link>
-    </footer>
-	)
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sessionStorage: sessionStorage
+    }
+  }
+
+  render() {
+    return(
+      <footer>
+        <Link to="/">Home</Link> |
+        <Display toggle={!(this.state.sessionStorage.length)}>
+          <Link to="/login">Login</Link> |
+          <Link to="/register">Register</Link>
+        </Display>
+      </footer>
+    )
+  }
 }
 
 export default Footer
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,16 +2,28 @@ import React from 'react'
 import Display from './Display'
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sessionStorage: sessionStorage
+    }
+    this.logOut = this.logOut.bind(this)
+  }
+
+  logOut() {
+    sessionStorage.clear()
+    window.location.replace("/");
+  }
+
   render() {
     return(
       <header>
         This is the header
-        <Display toggle={false}>
-          hello
+        <Display toggle={this.state.sessionStorage.length}>
+          <button onClick={this.logOut}>
+            Log Out
+          </button>
         </Display>
-        <button onClick={this.logOut}>
-          sessionStorage.clear();
-        </button>
       </header>
     )
   }
