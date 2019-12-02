22ba6d911bd00274c3b91d165db6e6096c06fc3d
BosEriko <bos.eriko@gmail.com>
2019-12-02 21:30:44 +0800

    [AUTO] Never gonna say goodbye

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
 import { Link } from 'react-router-dom'
+import Axios from 'axios'
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.deleteUser = this.deleteUser.bind(this)
+  }
+  deleteUser(event) {
+    Axios.delete(`http://localhost:4000/user/${this.props.match.params.user_id}/destroy`, {
+      headers: {
+        'x-auth-token': sessionStorage.token
+      }
+    }).then(function(response) {
+      sessionStorage.clear()
+      window.location.replace("/")
+    }).catch(function(error) {
+      console.log("Something went wrong.")
+    })
+    event.preventDefault()
+  }
   render() {
     const buttonStyle = "block w-full text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5"
     const buttonDangerStyle = "block w-full text-center bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5"
@@ -13,7 +32,7 @@ class Profile extends React.Component {
         </Helmet>
         <ul className="flex">
           <li className="mx-3 flex-1"><Link className={buttonStyle} to={`/profile/${this.props.match.params.user_id}/update`}>Update Profile</Link></li>
-          <li className="mx-3 flex-1"><Link className={buttonDangerStyle} to={`/profile/${this.props.match.params.user_id}/destroy`}>Delete Profile</Link></li>
+          <li className="mx-3 flex-1"><button className={buttonDangerStyle} onClick={this.deleteUser}>Delete Profile</button></li>
         </ul>
         { this.props.match.params.user_id }
       </div>
