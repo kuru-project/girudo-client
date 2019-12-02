3a82c4c6ec5dafc2814547a42e09788b7de17c6f
BosEriko <bos.eriko@gmail.com>
2019-12-02 20:30:05 +0800

    [AUTO] Whee, good night.

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -36,16 +36,18 @@ class Register extends React.Component {
   }
 
   render() {
+    const inputFieldStyle = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-5"
+    const buttonStyle     = "block w-full text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
     return(
       <div className="my-5">
         <Helmet>
           <title>Register</title>
         </Helmet>
         <form>
-          <input onChange={this.handleChange} name="name" placeholder="Name" />
-          <input onChange={this.handleChange} name="email" type="email" placeholder="Email" />
-          <input onChange={this.handleChange} name="password" type="password" placeholder="Password" current-password="true" autoComplete="off" />
-          <button onClick={this.handleSubmit}>Submit</button>
+          <input className={inputFieldStyle} onChange={this.handleChange} name="name" placeholder="Name" />
+          <input className={inputFieldStyle} onChange={this.handleChange} name="email" type="email" placeholder="Email" />
+          <input className={inputFieldStyle} onChange={this.handleChange} name="password" type="password" placeholder="Password" current-password="true" autoComplete="off" />
+          <button className={buttonStyle} onClick={this.handleSubmit}>Submit</button>
         </form>
       </div>
     )
