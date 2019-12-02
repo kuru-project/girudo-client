0370f310aa7e34c16ebfb7f7b5224ed7a0213255
BosEriko <bos.eriko@gmail.com>
2019-12-02 16:52:19 +0800

    [AUTO] bugger

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
   handleChange(event) {
-    console.log("Input is being updated")
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
   handleSubmit(event) {
-    console.log("You submitted the data!")
+    console.log("Email:", this.state.email)
+    console.log("Password:", this.state.password)
     event.preventDefault()
   }
   render() {
