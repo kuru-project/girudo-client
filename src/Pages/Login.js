6f1874d9576ac6b0b0911e81e7d99106aa31c25d
BosEriko <bos.eriko@gmail.com>
2019-12-04 05:11:23 +0800

    [AUTO] Continued development...

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -32,7 +32,8 @@ class Login extends React.Component {
       window.location.replace("/");
     }).catch(function(error) {
       createSnackbar('Something went wrong!', {
-        position: 'right'
+        position: 'right',
+        timeout: 2000
       })
     })
     event.preventDefault()
@@ -44,26 +45,27 @@ class Login extends React.Component {
         <Helmet>
           <title>Login</title>
         </Helmet>
-        <div className="text-center mb-5">
-          oh shit
-        </div>
         <div className="w-full max-w-sm mx-auto">
+          <div className="mb-5">
+            {/* Icon from: https://www.iconfinder.com/iconsets/japan-flat-2 */}
+            <img src="/icons/login.png" className="mx-auto max-w-full" alt="Log In" />
+          </div>
           <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div className="mb-4">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                 Email
               </label>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} type="email" id="email" placeholder="Email" />
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} name="email" type="email" id="email" placeholder="Email" />
             </div>
             <div className="mb-6">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                 Password
               </label>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} id="password" type="password" placeholder="Password" name="password" />
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} name="password" type="password" id="password" placeholder="Password" current-password="true" autoComplete="off" />
             </div>
             <div className="flex items-center justify-between">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={this.handleSubmit}>
-                Sign In
+                Login
               </button>
               <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" to="/register">
                 Register
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Axios from 'axios'
 import { Helmet } from 'react-helmet'
+import { createSnackbar } from '@snackbar/core'
+import '@snackbar/core/dist/snackbar.css'
+import { Link } from 'react-router-dom'
 
 class Register extends React.Component {
   constructor(props) {
@@ -28,7 +31,10 @@ class Register extends React.Component {
     }).then(function(response) {
       window.location.replace("/");
     }).catch(function(error) {
-      console.log("Something went wrong.")
+      createSnackbar('Something went wrong!', {
+        position: 'right',
+        timeout: 2000
+      })
     })
     event.preventDefault()
   }
@@ -47,6 +53,40 @@ class Register extends React.Component {
           <input className={inputFieldStyle} onChange={this.handleChange} name="password" type="password" placeholder="Password" current-password="true" autoComplete="off" />
           <button className={buttonStyle} onClick={this.handleSubmit}>Submit</button>
         </form>
+        <div className="w-full max-w-sm mx-auto">
+          <div className="mb-5">
+            {/* Icon from: https://www.iconfinder.com/iconsets/japan-flat-2 */}
+            <img src="/icons/login.png" className="mx-auto max-w-full" alt="Log In" />
+          </div>
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Name
+              </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} type="email" id="email" placeholder="Email" />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} type="email" id="email" placeholder="Email" />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Password
+              </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} id="password" type="password" placeholder="Password" name="password" />
+            </div>
+            <div className="flex items-center justify-between">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={this.handleSubmit}>
+                Login
+              </button>
+              <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" to="/register">
+                Register
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
