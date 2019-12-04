bfefe06a719c69d860c5470b0b5aa8d18d4b1694
BosEriko <bos.eriko@gmail.com>
2019-12-04 19:31:43 +0800

    [AUTO] Just committing so I can go home

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -85,6 +85,81 @@ class Book extends React.Component {
           </StripeProvider>
           <button className={buttonStyle} onClick={this.handleSubmit}>Book</button>
         </form>
+        <div className="w-full max-w-sm mx-auto">
+          <div className="mb-5">
+            {/* Icon from: https://www.iconfinder.com/iconsets/japan-flat-2 */}
+            <img src="/icons/update-profile.png" className="mx-auto max-w-full" alt="Update Profile" />
+          </div>
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                Full Name
+              </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} name="name" id="name" value={this.state.name} />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contact_number">
+                Contact Number
+              </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} name="contactNumber" id="contact_number" value={this.state.contactNumber} />
+            </div>
+            <div className="block text-gray-700 text-sm font-bold mb-2">Roles</div>
+            <div className="flex mb-4">
+              <div className="mr-5">
+                <label>
+                  <input
+                    name="isAdmin"
+                    type="checkbox"
+                    checked={this.state.isAdmin}
+                    onChange={this.handleChange} />
+                  <span className="ml-3">Admin</span>
+                </label>
+              </div>
+              <div>
+                <label>
+                  <input
+                    name="isArtist"
+                    type="checkbox"
+                    checked={this.state.isArtist}
+                    onChange={this.handleChange} />
+                  <span className="ml-3">Artist</span>
+                </label>
+              </div>
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="skill">
+                Skill
+              </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} name="skill" id="skill" value={this.state.skill} />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="profile_photo">
+                Profile Photo
+              </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} name="profilePhoto" id="profile_photo" value={this.state.profilePhoto} />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="cover_photo">
+                Cover Photo
+              </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} name="coverPhoto" id="cover_photo" value={this.state.coverPhoto} />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+                Description
+              </label>
+              <textarea className="resize-none shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={this.handleChange} name="description" id="description" value={this.state.description} />
+            </div>
+            <div className="flex items-center justify-between">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={this.handleSubmit}>
+                Update Profile
+              </button>
+              <button className="inline-block align-baseline font-bold text-sm text-red-500 hover:text-red-800" onClick={this.deleteUser}>
+                Delete Account
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
