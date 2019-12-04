763b442b6e2f9651c10a3d3fd6fe1fc62ac531f1
BosEriko <bos.eriko@gmail.com>
2019-12-04 08:37:36 +0800

    [AUTO] Is there an achievement for this?

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -69,27 +69,55 @@ class Profile extends React.Component {
   }
 
   render() {
-    const buttonStyle = "block w-full text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5"
-    const buttonBookStyle = "block w-full text-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5"
+    const buttonStyle = "text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 text-sm rounded focus:outline-none focus:shadow-outline"
+    const buttonBookStyle = "text-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-3 text-sm rounded focus:outline-none focus:shadow-outline"
     return (
       <div className="flex-1">
         <Helmet>
           <title>Profile</title>
         </Helmet>
-        <div className="bg-cover bg-gray-300 mb-5 h-32 md:h-48 lg:h-64 bg-center" style={{ backgroundImage: `url(${this.state.coverPhoto})` }}></div>
-        <div>{ this.state.profilePhoto }</div>
-        <div>{ this.state.description }</div>
-        <Display toggle={sessionStorage.length}>
-          <ul className="flex">
-              <Display toggle={this.state.currentUser && this.state.isArtist && !(this.state.currentUser.id === this.state.id)}>
-                <li className="mx-3 flex-1"><Link className={buttonBookStyle} to={`/book/${this.props.match.params.user_id}`}>Book</Link></li>
+        <div className="bg-cover bg-gray-300 mb-10 h-32 md:h-48 lg:h-64 bg-center" style={{ backgroundImage: `url(${this.state.coverPhoto})` }}></div>
+        <div className="flex items-center mb-5 container mx-auto">
+          <div className="mr-5 max-w-full w-64">
+            <img src={ this.state.profilePhoto } className="max-w-full rounded-full w-32 m-auto" alt={ this.state.name } />
+          </div>
+          <div className="flex-1 ml-5">
+            <div className="w-64 overflow-hidden max-w-full">
+              <h1 className="font-bold text-3xl">{ this.state.name }</h1>
+              <Display toggle={sessionStorage.length}>
+                <ul className="flex mb-5 mt-2">
+                    <Display toggle={this.state.currentUser && this.state.isArtist && !(this.state.currentUser.id === this.state.id)}>
+                      <li className="flex-1">
+                        <Link className={buttonBookStyle} to={`/book/${this.props.match.params.user_id}`}>
+                          <span className="mr-2"><i className="fas fa-book"></i></span>
+                          <span>Book</span>
+                        </Link>
+                      </li>
+                    </Display>
+                    <Display toggle={this.state.currentUser && this.state.currentUser.id === this.state.id}>
+                      <li className="flex-1">
+                        <Link className={buttonStyle} to={`/profile/${this.props.match.params.user_id}/update`}>
+                          <span className="mr-2"><i className="fas fa-cogs"></i></span>
+                          <span>Update Profile</span>
+                        </Link>
+                      </li>
+                    </Display>
+                </ul>
               </Display>
-              <Display toggle={this.state.currentUser && this.state.currentUser.id === this.state.id}>
-                <li className="mx-3 flex-1"><Link className={buttonStyle} to={`/profile/${this.props.match.params.user_id}/update`}>Update Profile</Link></li>
-              </Display>
-          </ul>
-        </Display>
-        { this.props.match.params.user_id }
+              <ul className="flex text-sm text-gray-500 mb-2">
+                <li className="mr-5">
+                  <span className="mr-2"><i className="fas fa-phone"></i></span>
+                  <span>{ this.state.contactNumber }</span>
+                </li>
+                <li>
+                  <span className="mr-2"><i className="fas fa-brain"></i></span>
+                  <span>{ this.state.skill }</span>
+                </li>
+              </ul>
+              <p>{ this.state.description }</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
