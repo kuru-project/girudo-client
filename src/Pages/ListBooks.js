31aa5c6723c00ff04bb29141153cdccd4e919b0a
BosEriko <bos.eriko@gmail.com>
2019-12-13 09:27:22 +0800

    add line

--- src/Pages/ListBooks.js
+++ src/Pages/ListBooks.js
@@ -37,17 +37,17 @@ class ListBooks extends React.Component {
             return (
               <li key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 overflow-hidden">
                 <Link to={`/profile/${ book.artist._id }`}>
-                  <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+                  <div className="max-w-sm rounded overflow-hidden hover:shadow-lg smooth shadow bg-white">
                     <div className="py-5 bg-cover bg-gray-300" style={{ backgroundImage: `url('${book.artist.coverPhoto}')` }}>
                       <img src={book.artist.profilePhoto} alt={book.artist.profilePhoto} className="rounded-full h-20 mx-auto" />
                     </div>
                     <div className="p-5">
                       <div className="mb-1"><span className="mr-1"><i className="far fa-calendar-alt"></i></span> { Moment(book.date).format("MMMM Do YYYY") || 'N/A' }</div>
                       <div className="mb-1"><span className="mr-1"><i className="fas fa-compass"></i></span> { book.location || 'N/A' }</div>
-                      <div className="mb-1"><span className="mr-1"><i className="fas fa-user"></i></span> { book.booker.name || 'N/A' }</div>
-                      <div className="mb-1"><span className="mr-1"><i className="fas fa-phone-alt"></i></span> { book.booker.contactNumber || 'N/A' }</div>
                       <div className="mb-1"><span className="mr-1"><i className="fas fa-stamp"></i></span> { book.artist.name || 'N/A' }</div>
-                      <div><span className="mr-1"><i className="fas fa-heart"></i></span> { book.artist.skill || 'N/A' }</div>
+                      <div className="mb-2 pb-2 border-b border-gray-300"><span className="mr-1"><i className="fas fa-heart"></i></span> { book.artist.skill || 'N/A' }</div>
+                      <div className="mb-1"><span className="mr-1"><i className="fas fa-user"></i></span> { book.booker.name || 'N/A' }</div>
+                      <div><span className="mr-1"><i className="fas fa-phone-alt"></i></span> { book.booker.contactNumber || 'N/A' }</div>
                     </div>
                   </div>
                 </Link>
